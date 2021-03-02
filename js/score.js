840936568bd328dbdd9a1917fa430a5a2a6ea6e8
Joni De Borger <joni.de.borger@ehb.be>
2021-03-02 13:32:01 +0100

    initial commit

---
+++ js/score.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//First implement with 1 feature: dropPosition
+
+//Step 1: 
+//Record (store) your data
+
+//Step 2:
+//Subtract the drop point from our 'to guess'-value (ex. 300px) and take the absolute value 
+
+//Step 3:
+//Sort
+
+//Step 4: 
+//Take the K-top records
+
+//Step 5: 
+//Calculate the most common bucket (check Lodash)
+
+//Now, how would you implement this using multiple features?
+
+const outputs = [];
+const K = 5;
+const predictionPoint = 300;
+
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  // Ran every time a balls drops into a bucket
+  console.log('Update!');
+}
+
+function runAnalysis() {
+  // Write code here to analyze stuff
+  console.log('Analyze!');
+}
+
+
